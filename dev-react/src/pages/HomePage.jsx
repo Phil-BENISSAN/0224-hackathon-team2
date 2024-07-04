adc7b3d03b82816382938df1f3fed0c8b01a732b
Rollet Fabien <fabrol47@gmail.com>
2024-07-04 09:56:47 +0200

    WIP home page

--- dev-react/src/pages/HomePage.jsx
+++ dev-react/src/pages/HomePage.jsx
@@ -3,15 +3,28 @@ import React, { useEffect, useState } from "react";
 import HeaderCard from "../components/HeaderCard";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import Review from "../components/Review";
 
 export default function HomePage() {
   return (
     <body>
       <main className="min-h-screen">
         <NavBar />
+        <div className="flex flex-col items-center bg-[url('https://i.kym-cdn.com/photos/images/newsfeed/001/871/712/85e.jpg')] bg-center h-[25rem] mb-16 ">
+          <h1 className="text-4xl font-bold m-10 mb-4">
+            Trouvez votre place dans le monde IT
+          </h1>
+          <p className="mb-5">
+            Votre plateforme de recrutement inclusif, basée sur vos compétences
+            réelles.
+          </p>
+          <Link className="btn btn-primary mx-auto px-8 mb-20">
+            Se connecter
+          </Link>
+        </div>
         <HeaderCard />
         <h2 className="text-center text-4xl font-bold m-10">
-          Ils nous font confiance
+          Nos clients sont notre meilleure garantie
         </h2>
         <ul className="flex flex-wrap justify-evenly">
           <li>
@@ -35,6 +48,9 @@ export default function HomePage() {
             <p>L’ORÉAL</p>
           </li>
         </ul>
+        <h2 className=" text-center text-4xl">Voir ce que les autres ont à dire</h2>
+        <p className=" text-center">Nos utilisateurs apprécient l'efficacité des tests ainsi que la qualité des retours reçus. Voici ce qu'ils disent de leur expérience.</p>
+        <Review/>
         <h2 className=" text-center text-4xl font-bold m-10">Le concept</h2>
         <div className="flex items-center">
           <p className="w-[30%] mx-[10%]">
