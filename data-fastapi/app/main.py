cc7389289bff087511cc403ddaa4225ddfbb56cd
arobert004 <anthony.robert.pro1@gmail.com>
2024-07-04 10:04:36 +0200

    adding softskills test

--- data-fastapi/app/main.py
+++ data-fastapi/app/main.py
@@ -1,15 +1,26 @@
 import uvicorn
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
+
+# ------- DATASETS IMPORTS -------------
+
+softskills = pd.read_csv('softskilltest.csv')
+
+# -------  INSERER VOTRE CODE ICI -----------------
 
 @app.get('/')
 def fn_fast_api():
 
-# -------  INSERER VOTRE CODE ICI -----------------
-
     return {'div' : "t'as tout compris"}
 
+
+@app.get('/softskills_test')
+def fn_softskills():
+    
+    return softskills.to_dict(orient='index')
+    
 
 # ---------------- FIN DE TON CODE ----------------
 #__________________________________________________
